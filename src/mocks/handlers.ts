84fe16d3d350bcb4fa444f26c51fceaccfba96c2
Nishant <mailtonis@hotmail.com>
2022-09-29 11:15:54 -0400

    mock data

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,9 @@
 // src/mocks/handlers.js
 import { rest } from 'msw'
+import { mockFoods } from './mockFoods'
 
 export const handlers = [
+
   rest.post('/login', (req, res, ctx) => {
     // Persist user's authentication in the session
     sessionStorage.setItem('is-authenticated', 'true')
@@ -29,42 +31,7 @@ export const handlers = [
     // If authenticated, return a mocked user details
     return res(
       ctx.status(200),
-      ctx.json([
-        {
-          "id": 1,
-          "name": "Burger",
-          "image": "burger.jpg",
-          "price": 8.99,
-          "description": "This ain't your average burger. Topped with our tangy cheddar cheese sauce, fresh lettuce, and tomato.",
-          "tags": [
-            "Lunch",
-            "Dinner"
-          ]
-        },
-        {
-          "id": 2,
-          "name": "Banana Blueberry French Toast",
-          "image": "banana-french-toast.jpg",
-          "price": 9.99,
-          "description": "Delicious french toast with banana and blueberry.",
-          "tags": [
-            "Breakfast"
-          ]
-        },
-        {
-          "id": 3,
-          "name": "Cajun Pasta",
-          "image": "cajun-pasta.jpg",
-          "price": 16.99,
-          "description": "Creole-style pasta that's guaranteed to make you sweat.",
-          "tags": [
-            "Lunch",
-            "Dinner",
-            "Spicy"
-          ]
-        }
-      ]
-       ),
+      ctx.json(mockFoods),
     )
   }),
 ]
